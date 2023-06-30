e3156fee21df4802d0e084b5ec24474f5f0240d6
Kannan <kannanvijayakumaran58@gmail.com>
2023-06-30 23:27:36 +0530

    adding two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int a=44;
+        int b=28;
+        int sum=a+b;
+        System.out.println(sum);
+
         System.out.println("Hello world!");
     }
 }
