e1ec55e9f6a18d0ab29a41ea019ced9b35cc7b11
Kannan <kannanvijayakumaran58@gmail.com>
2023-07-02 16:14:16 +0530

    lower case to upper case

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int a=44;
+
+        String lowercaseString = "hello world";
+        String uppercaseString = lowercaseString.toUpperCase();
+
+        System.out.println(uppercaseString);  // Output: HELLO WORLD
+
         int b=28;
         int sum=a+b;
         System.out.println(sum);
